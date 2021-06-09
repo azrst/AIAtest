669be3fe0b9f12344e6d8b7c9aa167b867a19318
Muhammad Aziz Syafryadi <muhammad.aziz@erloom.id>
2021-06-09 15:33:41 +0700

    commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {
+  View,
+  Text
+} from 'react-native'
+export class App extends Component {
+  render() {
+    return (
+      <View style={{flex : 1, backgroundColor : 'white', justifyContent : 'center', alignItems : 'center'}}>
+        <Text>app.js</Text>
+      </View>
+    )
+  }
+}
+
+export default App
